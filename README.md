8e1e0d5c377ad4ef71b0741e99f94afb0d5f363d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-05 23:33:46 +0300

    retrieve items

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 - HTTP networking course with fetch &amp; rest in JS
 
 # [Free Code Camp course](https://www.youtube.com/watch?v=2JYT5f2isg4) 
+
+1. retrieve-items
---
+++ retrieve-items.js
@@ -0,0 +1,21 @@
+const itemURL = 'https://api.boot.dev/v1/courses_rest_api/learn-http/items';
+
+const items = await getData(itemURL);
+
+items.forEach(item => {
+    console.log(item.name);
+});
+
+async function getData(url) {
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-API-Key': 'Testing',
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.json();
+}
+
