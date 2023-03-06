1cf6ccc6977e2f69c2f574bfbcf77f7dbdcce000
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-06 17:33:23 +0300

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -6,7 +6,7 @@ function normalizeURL(urlString) {
 
     if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
         return hostPath.slice(0, -1)
-    } 
+    }
     return hostPath
 }
 
@@ -38,26 +38,57 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls
 }
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
 
+    // if this is an offsite URL, bail immediately
+    const currentUrlObj = new URL(currentURL)
+    const baseUrlObj = new URL(baseURL)
+    if (currentUrlObj.hostname !== baseUrlObj.hostname) {
+        return pages
+    }
+
+    const normalizedURL = normalizeURL(currentURL)
+
+    // if we've already visited this page
+    // just increase the count and don't repeat
+    // the http request
+    if (pages[normalizedURL] > 0) {
+        pages[normalizedURL]++
+        return pages
+    }
+
+    // initialize this page in the map
+    // since it doesn't exist yet
+    pages[normalizedURL] = 1
+
+    // fetch and parse the html of the currentURL
+    console.log(`crawling ${currentURL}`)
+    let htmlBody = ''
     try {
         const resp = await fetch(currentURL)
-
         if (resp.status > 399) {
             console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`Non-html response, content-type: ${contentType} on page ${currentURL}`)
-            return
+            return pages
         }
+        htmlBody = await resp.text()
 
-        console.log(await resp.text())
     } catch (error) {
-        console.log(`Error in fetch: ${error}`)    
+        console.log(`Error in fetch: ${error}`)
     }
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
+    return pages
 }
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./crawl.js')
 
-function main() {
+async function main() {
     if (process.argv.length < 3 || process.argv.length > 3) {
         console.log("Invalid argument count!")
         process.exit(1)
@@ -9,7 +9,11 @@ function main() {
     const baseURL = process.argv[2]
 
     console.log(`Starting crawl of ${baseURL}`)
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    for (const page of Object.entries(pages)) {
+        console.log(page)
+    }
 }
 
 main()
