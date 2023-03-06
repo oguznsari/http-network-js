9f4ad430f3f018c8f780a8a3118884ea0b868e5d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-03-06 12:18:16 +0300

    normalizeURL and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,34 @@
 const { normalizeURL } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
-test('normalizeURL', () => {
-    const input = ''
+test('normalizeURL strip Protocol', () => {
+    const input = 'https://blog.boot.dev/path'
     const output = normalizeURL(input)
-    const expected = ''
+    const expected = 'blog.boot.dev/path'
+
+    expect(output).toEqual(expected)
+})
+
+test('normalizeURL strip Protocol(http)', () => {
+    const input = 'http://BLOG.boot.dev/path'
+    const output = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+
+    expect(output).toEqual(expected)
+})
+
+test('normalizeURL strip trailing slash ', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const output = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+
+    expect(output).toEqual(expected)
+})
+
+test('normalizeURL strip CAPITALS ', () => {
+    const input = 'https://BLOG.boot.dev/path'
+    const output = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
 
     expect(output).toEqual(expected)
 })
