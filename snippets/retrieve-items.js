a53b7de63f484d90c34208cabd9255ed12a1b65f
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-03-11 11:27:18 +0300

    vs code format on save config - prettier

--- snippets/retrieve-items.js
+++ snippets/retrieve-items.js
@@ -1,21 +1,20 @@
-const itemURL = 'https://api.boot.dev/v1/courses_rest_api/learn-http/items';
+const itemURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/items";
 
 const items = await getData(itemURL);
 
-items.forEach(item => {
-    console.log(item.name);
+items.forEach((item) => {
+  console.log(item.name);
 });
 
 async function getData(url) {
-    const response = await fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-API-Key': 'Testing',
-            'Content-Type': 'application/json'
-        }
-    });
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "X-API-Key": "Testing",
+      "Content-Type": "application/json",
+    },
+  });
 
-    return response.json();
+  return response.json();
 }
-
